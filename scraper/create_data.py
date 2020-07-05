0c4343028b2beaab8f8562936f627ce026f809e0
Jessica <jessly5@users.noreply.github.com>
2020-07-05 02:37:49 -0400

    checks for program restriction instead of just mgd/dem, and better tagging

--- scraper/create_data.py
+++ scraper/create_data.py
@@ -1,4 +1,5 @@
 
+import re
 import json
 import urllib.request
 from bs4 import BeautifulSoup
@@ -13,24 +14,26 @@ format example:
         'coursePrerequisites': [
                 'CSC108H5'
         ],
-        'tags': 'programming'
+        'tags': 'Programming'
     }
 }
+
 '''
 
 
 def scrape():
     print('collecting data')
     
-    page_url = "https://student.utm.utoronto.ca/timetable/timetable?yos=&subjectarea=4&session=20199&courseCode=&sname=&delivery=&courseTitle="
+    page_url = "https://student.utm.utoronto.ca/timetable/timetable?yos=&subjectarea=4&session=20209&courseCode=&sname=&delivery=&courseTitle="
 
     page = urllib.request.urlopen(page_url)
     soup = BeautifulSoup(page, 'html.parser')
 
     course_titles = soup.find_all('h4')
     course_details = soup.find_all('div', attrs={'class': 'infoCourseDetails'})
+    course_control = soup.find_all('div', attrs={'class': 'enrollment_control'})
 
-    result = [course_titles, course_details]
+    result = [course_titles, course_details, course_control]
     print('finished collecting data')
     return result
 
@@ -39,7 +42,10 @@ def modify(result):
     data = {}
     course_titles = result[0]
     course_details = result[1]
+    course_control = result[2]
+    wri_hack = 0
     skip = 0
+    ccit_program = 'MA COMM, CULTURE, INFO & TECH'
 
     print("formatting data\n------")
     
@@ -48,9 +54,16 @@ def modify(result):
         #assumption: course codes are in the standard ABC123 + H/Y + 5 + F/W format
         course_code = course_title_full[0:8]
 
-        if ('MGD' in course_code or 'WRI' in course_code):
-            print('skipping course ', i+1, ' of ', len(course_titles), ' because it is MGD/WRI')
+        #+1 since .find() returns -1 if not found but evaluates to True, and that is not what we contextually interpret it as
+        ccit_can_take = True if course_control[i-wri_hack].text.strip().find(ccit_program)+1 or "CCT1" in course_code else False
+
+        if ('WRI' in course_code or (not ccit_can_take)):
+            print('skipping course ', i+1, ' of ', len(course_titles), ' because it is WRI or restriction excludes CCIT major')
             skip += 1;
+            
+            #hard coded workaround for the 2 courses that currently don't have any restrictions
+            if ('WRI307H5S' in course_title_full or 'WRI490H5F' in course_title_full):
+                wri_hack += 1
             
         else:
             course_detail = course_details[i].text.strip()
@@ -72,13 +85,18 @@ def modify(result):
 
 
 def tag(course_detail):
-    tags = ['coding', 'software', 'design', 'experience', 'perception', 'research', 'political', 'ethics', 'managing', 'media', 'mass', 'advertising']
+    regex_tags = ['[Pp]rogram', '[Ss]oftware', '[Dd]esign', '[Ii]nternship', '[Hh]uman', '[Rr]esearch', '[Pp]olit', \
+            '[Ee]xperiential', '[Mm]anag', '[Mm]edia', '[Mm]arket', '[Aa]dvertis']
+    tags = ['Programming', 'Software', 'Design', 'Internship', 'Human', 'Research', 'Politics', \
+            'Experiential', 'Management', 'Media', 'Market', 'Advertising']
     relevant_tags = []
     
-    for t in tags:
-        if t in course_detail:
-            relevant_tags.append(t)
-    
+    for i in range (len(regex_tags)):
+        search_result = re.search(regex_tags[i], course_detail)
+        if (search_result):
+            relevant_tags.append(tags[i])
+
+    relevant_tags = relevant_tags if relevant_tags else "No Tags"
     return relevant_tags
 
 
