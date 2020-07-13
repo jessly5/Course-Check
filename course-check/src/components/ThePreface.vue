b54dc5a85ece59a8a29a410939273c83e8388516
Jessica <jessly5@users.noreply.github.com>
2020-07-12 22:08:11 -0400

    preface text edit

--- course-check/src/components/ThePreface.vue
+++ course-check/src/components/ThePreface.vue
@@ -3,10 +3,10 @@
 		<h1>Course Checker</h1>
 
 		<h2>What is this?</h2>
-		<p>This tool displays and filters data scraped from the UTM timetable for the 2020-2021 fall/winter session. It currently focuses on courses for the CCIT major, which excludes any course not beginning with CCT and any course that has a restriction to exclude CCIT majors.</p>		
+		<p>This tool displays and filters data scraped from the UTM timetable for the 2020-2021 fall/winter session (with the exception of a hard coded course that is only offered in the summer). It currently focuses on courses for the CCIT major, which excludes any course not beginning with CCT and any course that has a restriction to exclude CCIT majors.</p>		
 			
 		<h2>Why is this a thing?</h2>
-		<p>Currently the timetable displays all the ICCIT courses under one subject area, even though ICCIT currently hosts three programs. The courses are all together in alphabetical order due to being under the same subject area filter. It's annoying scrolling through courses I am restricted from taking or just simply not in my program. For example, most courses codes starting with MGD or include DEM in the title will not allow CCIT majors to enroll. In addition, PWC program courses are always listed at the bottom since they start with WRI. I've emailed the UTM Registrar about my complaint, but I do not know if they will fix it.</p>
+		<p>Currently the official UTM timetable displays CCIT, DEM, and PWC program courses under on subject area due to them being offered by the same department. However, I am only interested in courses that a CCIT major can take. Having to scroll through courses I am restricted from taking or simply not for my program is annoying. For example, most course codes starting with MGD or include DEM in the title will not allow CCIT majors to enroll.</p>
 	</div>
 </template>
 
